964a7533e7e4533c38246c5b650faf799a59940f
Timothy Pendarvis <trpendarvis@gmail.com>
2022-08-04 15:47:38 -0600

    Save work in functions.js

--- functions.js
+++ functions.js
@@ -1,10 +1,18 @@
-// 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
-
+// 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called.
+// Call this function 3 times.
+function printGreeting () {
+}
+  console.log("Hello friend")
+  console.log("Hello friend")
+  console.log("Hello friend")
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 
 
-// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company,
+// and the numbers will represent the minimum and maximum of a pay range for a posted job.
+// The function should print out a sentence that includes the name of the company and the range itself
+// (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
 
 // 4: Write a function that satifies the following interaction pattern:
